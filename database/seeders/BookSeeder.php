f14a7be9eaca15b92a853ce58bd6ecbf2f2c3e0a
Zharkynuly Yedil <62800476+yed1l@users.noreply.github.com>
2021-07-18 20:53:50 +0600

    some changes in seeds

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -32,9 +32,41 @@ class BookSeeder extends Seeder
                 'price' => '450000',
                 'book_image' => 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back02.jpg',
                 'book_description' => 'Новый',
-                'created_at'=> '2020-07-16 12:20:22'
+                'created_at'=> '2021-07-10 12:20:22'
             ],
 
+            [
+                'id' => 3,
+                'book_name' => 'Chair',
+                'price' => '5000',
+                'book_image' => 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back02.jpg',
+                'book_description' => 'Новый',
+                'created_at'=> '2021-07-12 14:20:22'
+            ],
+            [
+                'id' => 4,
+                'book_name' => 'laptop',
+                'price' => '200000',
+                'book_image' => 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back02.jpg',
+                'book_description' => 'БУ',
+                'created_at'=> '2021-07-10 10:20:22'
+            ],
+            [
+                'id' => 5,
+                'book_name' => 'Airpods 2',
+                'price' => '49999',
+                'book_image' => 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back02.jpg',
+                'book_description' => 'Идеальное состояние',
+                'created_at'=> '2021-06-10 12:20:22'
+            ],
+            [
+                'id' => 6,
+                'book_name' => 'Camry 75',
+                'price' => '1050000',
+                'book_image' => 'http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_1.png',
+                'book_description' => 'Новый',
+                'created_at'=> '2021-07-02 12:20:22'
+            ],
         ]);
 
     }
